02c5b967229c8c84a4c96d1c8ae95bfa343b3275
정광근 <carrot.jung@jeong-gwang-geuns-MacBook-Pro.local>
2025-02-06 21:14:40 +0700

    concurrent colloection

---
+++ src/main/java/org/example/collection/simple/SimpleListMainV0.java
@@ -0,0 +1,9 @@
+package org.example.collection.simple;
+
+public class SimpleListMainV0 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/example/collection/simple/list/SimpleListMainV1.java
@@ -0,0 +1,12 @@
+package org.example.collection.simple.list;
+
+public class SimpleListMainV1 {
+
+    public static void main(String[] args) {
+        SimpleList list = new BasicList();
+        list.add("A");
+        list.add("B");
+        System.out.println("list = " + list);
+    }
+
+}
